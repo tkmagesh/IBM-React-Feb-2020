657c55b5beb41589933dc5cb7aa590405c462017
Magesh Kuppan <tkmagesh77@gmail.com>
2020-02-05 15:03:34 +0530

    refactoring the store

--- 03-products-app/src/index.js
+++ 03-products-app/src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import { createStore, bindActionCreators, combineReducers } from 'redux';
-import productsReducer from './Products/reducers';
-
-import Products from './Products';
 import { Provider } from 'react-redux';
 
-import  Categories , { categoriesReducer } from './Categories';
-
-const rootReducer = combineReducers({
-    productsState : productsReducer,
-    categoriesState : categoriesReducer
-});
-
-var store = createStore(rootReducer);
+import Products from './Products';
+import Categories from './Categories';
+import appStore from './store';
 
 ReactDOM.render( 
-    <Provider store={store}>
+    <Provider store={appStore}>
         <div>
             <Categories />
             <hr/>
---
+++ 03-products-app/src/store/index.js
@@ -0,0 +1,13 @@
+import { combineReducers, createStore } from 'redux';
+
+import { productsReducer } from '../Products/reducers';
+import { categoriesReducer } from '../Categories';
+
+const rootReducer = combineReducers({
+    productsState: productsReducer,
+    categoriesState: categoriesReducer
+});
+
+var store = createStore(rootReducer);
+
+export default store;
